3f67f2c4682ba56d195c9c175961948d97bbb139
sumo89 <apple@Henrys-Macbook-Pro.local>
2018-07-15 18:36:03 +0100

    es6 & less grunt boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                options: {
+                paths: ['assets/css']
+                },
+                files: {
+                'assets/main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                paths: ['assets/css'],
+                plugins: [],
+                modifyVars: {}
+                },
+                files: {
+                'assets/main.css': 'main.less'
+                }
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    'dist/app.js': 'app.es6'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['*.es6', '*.less'],
+                tasks: ['default'],
+                options: {
+                spawn: false,
+                },
+            },
+        },
+        connect: {
+            server: {
+                options: {
+                    keepalive: true,
+                    livereload: true,
+                    port: 9001,
+                    base: ''
+                }
+            }
+        }
+      
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('serve', ['connect']);
+    grunt.registerTask('default', ['less'], ['babel']);
+  
+  };
---
+++ dist/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('hi');
+//# sourceMappingURL=app.js.map
